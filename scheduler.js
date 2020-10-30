58b9deb5d64414ab7c6b72499e81e8ff3fd3715d
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2020-10-30 15:05:07 -0500

    Created color coded for the past, present, and future time in the schedule textarea

--- scheduler.js
+++ scheduler.js
@@ -1,53 +1,72 @@
-// Method to make the function run after the page is ready.
+// Method to make the functions run after the page is ready.
 $( document ).ready(function() {
 
+    // Call the getCurrentDay and createTimeBlocks functions.
     getCurrentDay();
+    createTimeBlocks(); 
 
-    // Time blocks for strandard business hours
+});
+
+// Function for when the user open the planner the current day is displayed at the top of the calendar.
+function getCurrentDay() {
+    let currentDay = moment().format("dddd, MMMM Do");
+    $("#currentDay").text(currentDay);
+}
+
+// Time blocks for strandard business hours.
+function createTimeBlocks() {
+
+    // Create new variable for grabbing the current hours
+    let currentHour = moment().format("HH");
+    console.log(currentHour);
+    
     // Looping time 9am-5am.
     // start with 9 for 9am and with the length from 9-18.
     for (let i=9; i < 18; i++) {
 
         // Create a row for schedule blocks.
-        // Add class and add id with the time index, so it will be convenient to call the row.
+        // Add class, set data time, and add id with the time index, so it will be convenient to call the row.
         let timeBlockRow = $("<div>");
         timeBlockRow.addClass("row");
+        timeBlockRow.attr("data-time", `${i}`);
         timeBlockRow.attr("id", `row${i}`);
 
         // Create a hours column.
         // Add classes and set the text to the 9am-5pm.
-        // Create a new variable for time format to be put inside the hours column.
         let hourColEl = $("<div>");
         hourColEl.addClass("col-sm-2 hour");
-        
+
+        // Create if statement for the hour AM/PM format to be put inside the hours column.
         if (i <= 11) {
-            hourColEl.text(i + "AM");
+            hourColEl.text(i + "am");
         }
         else if (i === 12) {
-            hourColEl.text(i + "PM");
+            hourColEl.text(i + "pm");
         }
         else {
-            hourColEl.text(i-12 + "PM");
+            hourColEl.text(i-12 + "pm");
         };
 
         // Create a schedule column.
-        // Add classes, textarea tag, and id for the textarea.
-        // Append the schedule textarea to the schedule column div
-        let scheduleColEl = $("<div>");
-        scheduleColEl.addClass("col-sm-8 past");
+        // Add textarea element, classes and id for the textarea.
+        let scheduleColEl = $("<textarea>");
+        scheduleColEl.addClass("col-sm-8 past description");
+        scheduleColEl.attr("id", "text");
 
-        let scheduleTextEl = $("<textarea>");
-        scheduleTextEl.addClass("description");
-        scheduleTextEl.attr("id", `text${i}`);
-
-        scheduleColEl.append(scheduleTextEl);
-
+        // if statement to decide which color to display for the past, present, and future schedules.
+        if ( timeBlockRow.data("time") == currentHour ) {
+            
+            scheduleColEl.addClass("present");
+        }
+        else if ( currentHour < timeBlockRow.data("time") ) {
+          
+            scheduleColEl.addClass("future");
+        }
 
         // Create a save button column.
-        // Add class, button tag, and id into it.
+        // Add class and id into it.
         // Add save logo inside the button.
         // Append save button logo to the button tag.
-        // Append save button element to the save button parent column.
         let saveBtnColEl = $("<button>");
         saveBtnColEl.addClass("col-md-1 saveBtn");
         saveBtnColEl.attr("id", `${i}`);
@@ -57,18 +76,12 @@ $( document ).ready(function() {
 
         saveBtnColEl.append(saveLogoEl);
 
-
         // Append hour column, schedule column, and save button column to the time block row.
         timeBlockRow.append(hourColEl);
         timeBlockRow.append(scheduleColEl);
         timeBlockRow.append(saveBtnColEl);
 
+        // Append all the collected elements to the class container.
         $(".container").append(timeBlockRow);
     };
-});
-
-// When the user open the planner the current day is displayed at the top of the calendar.
-function getCurrentDay() {
-    let currentDay = moment().format("dddd, MMMM Do");
-    $("#currentDay").text(currentDay);
 }
